9edeba158e373f56d0d571c61ebe19ddeff2c016
maikon-parreira <maikon.parreira@talkdesk.com>
2023-01-05 11:04:16 +0000

    first commit

---
+++ src/main/kotlin/com/github/mknparreira/kotlinservice/KotlinserviceApplication.kt
@@ -0,0 +1,11 @@
+package com.github.mknparreira.kotlinservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinserviceApplication
+
+fun main(args: Array<String>) {
+	runApplication<KotlinserviceApplication>(*args)
+}
---
+++ src/test/kotlin/com/github/mknparreira/kotlinservice/KotlinserviceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.github.mknparreira.kotlinservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinserviceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
